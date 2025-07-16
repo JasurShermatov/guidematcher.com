fbc784dc77df8b5c92d76ffcf1acf29ef867bd09
Jasur <shermatovjasur800@gmail.com>
2025-07-16 15:23:41 +0500

    account model is written

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,1 +1,15 @@
-a
+from django.db import models
+from django.utils import timezone
+
+class EmailVerification(models.Model):
+    email = models.EmailField()
+    code = models.CharField(max_length=6)
+    is_used = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def is_expired(self):
+        return self.expires_at < timezone.now()
+
+    def __str__(self):
+        return f"{self.email} - {self.code} ({'used' if self.is_used else 'active'})"

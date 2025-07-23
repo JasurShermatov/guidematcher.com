aec71feedb7e445a8952d59484fadc900035e330
Jasur <shermatovjasur800@gmail.com>
2025-07-23 15:49:31 +0500

    update UserAdmin: add admin registration, improve list filters, and update search fields

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,12 @@
-
+from django.contrib import admin
 from .models import User
+
+
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-        "email",
-        "first_name",
-        "last_name",
-        "is_staff",
-    )
+    list_filter = ("email",
+                   "first_name",
+                   "last_name",
+                   "is_staff",
+                   )
     search_fields = ("email", "first_name", "last_name")

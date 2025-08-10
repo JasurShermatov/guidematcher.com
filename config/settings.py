a356173e3bd68858ab4f26cfe13839b3a8f93e3c
Shaxzodbek/ <muxtorovshaxzodbek16@gmail.com>
2025-08-11 04:58:17 +0500

    🔒 chore: set JWT access token lifetime to 30min

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30 * 48 * 31 * 12),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,

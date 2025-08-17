6029df76bf5d604a06ad6210488d155d010092d5
Jasur <shermatovjasur800@gmail.com>
2025-08-17 05:04:07 +0500

    ✨ feat: refactor user management by renaming profile viewset to user viewset and implementing ownership permissions

---
+++ apps/users/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerOrAdmin(BasePermission):
+    """
+    - Admin → hamma userlarga kirishi mumkin
+    - Oddiy user → faqat o‘z profilini ko‘rishi va yangilashi mumkin
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Admin → har doim ruxsat
+        if request.user.is_staff or request.user.is_superuser:
+            return True
+
+        # Oddiy user → faqat o‘ziga
+        return obj == request.user

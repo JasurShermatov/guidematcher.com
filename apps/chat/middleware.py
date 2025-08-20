e9e395dacd03b2f148c3843c477789a70f523554
Jasur <shermatovjasur800@gmail.com>
2025-08-21 02:19:44 +0500

    ✨ feat: simplify chat URL structure and refactor chat models for improved clarity and functionality

--- apps/chat/middleware.py
+++
@@ -1,8 +0,0 @@
-from channels.auth import AuthMiddlewareStack
-from .auth import QueryStringJWTAuthMiddleware, RoomPermissionMiddleware
-
-
-def JWTAuthMiddlewareStack(inner):
-    return QueryStringJWTAuthMiddleware(
-        RoomPermissionMiddleware(AuthMiddlewareStack(inner))
-    )
